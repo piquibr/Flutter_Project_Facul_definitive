f64d57a39f18269654d85d2083c8bf30d5cc9808
Manoel-DJS <94149934+Manoel-DJS@users.noreply.github.com>
2024-11-29 15:53:28 -0300

    config banco reserva

--- express-firebase-api/app.js
+++ express-firebase-api/app.js
@@ -28,7 +28,21 @@ const transporter = nodemailer.createTransport({
 });
 
 // Inicialize o Firebase Admin com a chave privada
-const serviceAccount = require("./google-services.json");
+require('dotenv').config();
+// const serviceAccount = require("./google-services.json");
+
+const serviceAccount = {
+  type: process.env.SERVICE_ACCOUNT_TYPE,
+  project_id: process.env.SERVICE_ACCOUNT_PROJECT_ID,
+  private_key_id: process.env.SERVICE_ACCOUNT_PRIVATE_KEY_ID,
+  private_key: process.env.SERVICE_ACCOUNT_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  client_email: process.env.SERVICE_ACCOUNT_CLIENT_EMAIL,
+  client_id: process.env.SERVICE_ACCOUNT_CLIENT_ID,
+  auth_uri: process.env.SERVICE_ACCOUNT_AUTH_URI,
+  token_uri: process.env.SERVICE_ACCOUNT_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.SERVICE_ACCOUNT_AUTH_PROVIDER_CERT_URL,
+  client_x509_cert_url: process.env.SERVICE_ACCOUNT_CLIENT_CERT_URL
+};
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
--- express-firebase-api/config/configFirebase.js
+++ express-firebase-api/config/configFirebase.js
@@ -8,13 +8,16 @@ import { getAnalytics } from "firebase/analytics";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+require('dotenv').config();
+
 const firebaseConfig = {
-  apiKey: "AIzaSyAq9fhnLycQGqhieg3isiU2uSStZEbphM8",
-  authDomain: "todoolistnv.firebaseapp.com",
-  projectId: "todoolistnv",
-  storageBucket: "todoolistnv.firebasestorage.app",
-  messagingSenderId: "346458540439",
-  appId: "1:346458540439:web:c787c2a96721608c711cd2"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
